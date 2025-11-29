85de6918f061836311df77def74964e16a0976b8
MauricioPJ06 <166061053+MauricioPJ06@users.noreply.github.com>
2025-11-28 22:46:38 -0500

    Nombres Actualizados

--- src/entity/Cliente.java
+++ src/entity/Cliente.java
@@ -26,24 +26,59 @@ public class Cliente {
     }
 
     // Getters y Setters
-    public int getIdCliente() { return idCliente; }
-    public void setIdCliente(int idCliente) { this.idCliente = idCliente; }
+    public int getIdCliente() {
+        return idCliente;
+    }
+    
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
 
-    public String getDNI() { return DNI; }
-    public void setDNI(String DNI) { this.DNI = DNI; }
+    public String getDNI() {
+        return DNI;
+    }
+    
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
 
-    public String getNombres() { return nombres; } 
-    public void setNombres(String nombres) { this.nombres = nombres; } 
+    public String getNombres() {
+        return nombres;
+    } 
+    
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    } 
 
-    public String getApellidos() { return apellidos; }
-    public void setApellidos(String apellidos) { this.apellidos = apellidos; }
+    public String getApellidos() {
+        return apellidos;
+    }
+    
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
 
-    public String getTelefono() { return telefono; }
-    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public String getTelefono() {
+        return telefono;
+    }
+    
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    public String getNumeroContrato() { return numeroContrato; }
-    public void setNumeroContrato(String numeroContrato) { this.numeroContrato = numeroContrato; }
+    public String getNumeroContrato() {
+        return numeroContrato;
+    }
+    
+    public void setNumeroContrato(String numeroContrato) {
+        this.numeroContrato = numeroContrato;
+    }
 }
